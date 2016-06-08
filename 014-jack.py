ab4fcf027778555b8a0709a72491288b50f41a12
dream0892 <dream0892@163.com>
2016-06-08 09:53:30 +0800

    add 014-jack.py

---
+++ 014-jack.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 纯文本文件 student.txt为学生信息, 里面的内容（包括花括号）如下所示：
+import json
+import pyexcel
+import pdb
+f=open('student.txt')
+d_student=json.load(f)
+#pdb.set_trace()
+sheet=pyexcel.get_sheet(adict=d_student)
+sheet.name='student'
+sheet.transpose()
+sheet.save_as('student.xlsx')
